ee200d4a9288b467a945f8b44a12a27fccecb44d
secke <bocarkeita2010@gmail.com>
2022-07-11 01:08:30 +0000

    test commit 001

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import streamlit as st
 st.title("CECI EST UN SIMPLE TEST AVEC STREAMLIT")
 
